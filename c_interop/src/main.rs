114fe1161a2e62f9bb1f90558bb033839320ed08
SirMorfield <joppe@joppekoers.nl>
2023-09-01 22:54:21 +0200

    Add rt files

--- c_interop/src/main.rs
+++ c_interop/src/main.rs
@@ -1,7 +1,7 @@
 use std::os::raw::c_char;
 use std::os::raw::c_int;
 mod helpers;
-use helpers::vector_to_cstring_vector;
+use helpers::*;
 
 extern {
 	fn mainj(
@@ -15,8 +15,10 @@ fn main() {
 	let result: i32;
     unsafe {
 		let argv = std::env::args().collect::<Vec<_>>();
-
-        result = mainj(0, vector_to_cstring_vector(argv));
+		for arg in &argv {
+			println!("The arg is: {}", arg);
+		}
+        result = mainj(argv.len() as i32, vector_to_cstring_vector(argv));
     }
     println!("The result from C is: {}", result);
 }
